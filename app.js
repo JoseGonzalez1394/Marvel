0a5a9bf75e872f29f387b1a2e584045b14264ebd
Jose A Gonzalez <JGman13@gmail.com>
2022-07-01 16:10:07 -1000

    I passed?

--- app.js
+++ app.js
@@ -1,43 +1,41 @@
-let yellow = "Yellow";
-let otherYellow = "yellow"; 
+function askMarvelmovies() {
+    let movies = prompt('How man Marvel movies are out?');
+    let message;
+    if (movies <= 10) {
+        message = 'Way Moooore!';
+    } else if (movies <= 20) {
+        message = 'Almost there!';
+    } else if (movies <= 30) {
+        message = 'Almost almost there';
+    } else {
+        message = 'You know your stuff!';
+        askMarvelmovies();
+    }
+    document.write(message);
+    // return message;
+};
 
-console.log(yellow == otherYellow);
-
-console.logprompt==("What is your favorite color?");
-
-let usersColor = prompt("What is your favorite Avenger?");
-console.log(usersColor);
-olor = yellow
-
-console.log(yellow);
-console.l
-
-let x = 5; 
-let y ="5"; 
-
-console.log(x,y); // number, string
-console.log(x == y); // equals
-console.log(x === y); // STRICTLY equals
-console.log(x = y); // reassign
-console.log(x);
-
-//if the user's fav color is yellow, say "Mine too!"
-// otherwise say "That's a good color"
-
-//if (usersColor == "YELLOW"){
-//    console.log("I also like YELLOW!!!");
-//} else if (usersColor.toLowerCase() == ("yellow") {
-//    console.log("Mine too!");
-//} 
-//else  {
-//    console.log("That's still a nice color.");
-// }
-
- if (usersColor.toLowerCase()== "Iron Man"){
-    console.log("I am Iron Man");
-} else if (usersColor()== "Thor") {
- console.log("Well he is a Pirate Angel"); 
-} else {
-    console.log("That's still a nice color.");
+// askMarvelmovies();
+function addNumbers(x, y) {
+    return x + y;
 }
 
+function favAvenger() {
+    let usersAvenger = prompt("Who is your favorite Avenger?").toLowerCase();
+    if (usersAvenger === "iron man") {
+        document.write("I am Iron Man!");
+    } else if (usersAvenger === "thor") {
+        document.write("Well he is a space angel!");
+    } else {
+        document.write("That's a good one.");
+    }
+}
+
+// favAvenger();38
+
+function frustration() {
+    const userNum = prompt("How did I feel making this?");
+    // for (let i = 0; i < userNum; i++) {
+        document.write("<iframe src=\"https://giphy.com/embed/l9Jhzwdi09Ve0\" width=\"480\" height=\"366\" frameBorder=\"0\" class=\"giphy-embed\" allowFullScreen></iframe>");
+    document.write(username);
+}
